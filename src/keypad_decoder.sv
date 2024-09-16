03ba4320f9f995e33bb760bad61c9bc6265dfb87
Troy Kaufman <tkaufman@hmc.edu>
2024-09-15 21:32:38 -0700

    All simulations work

--- src/keypad_decoder.sv
+++ src/keypad_decoder.sv
@@ -8,41 +8,41 @@ module keypad_decoder(input logic [3:0] col_d, row_d,
 
     always_comb begin
         case(col_d)
-            0001: case (row_d)
-                    0001: begin key_pushed = 'ha; encoded_row_q = 'd1; end
-                    0010: begin key_pushed = 'h7; encoded_row_q = 'd2; end
-                    0100: begin key_pushed = 'h4; encoded_row_q = 'd3; end
-                    1000: begin key_pushed = 'h1; encoded_row_q = 'd4; end
-                    default: key_pushed = 'h0;
+            4'b0001: case (row_d)
+                    4'b0001: begin key_pushed = 'ha; encoded_row_q = 2'b00; end
+                    4'b0010: begin key_pushed = 'h7; encoded_row_q = 2'b01; end
+                    4'b0100: begin key_pushed = 'h4; encoded_row_q = 2'b10; end
+                    4'b1000: begin key_pushed = 'h1; encoded_row_q = 2'b11; end
+                    default: key_pushed = 'h4;
                   endcase
-            0010: case (row_d)
-                    0001: begin key_pushed = 'h0; encoded_row_q = 'd1; end
-                    0010: begin key_pushed = 'h8; encoded_row_q = 'd2; end
-                    0100: begin key_pushed = 'h5; encoded_row_q = 'd3; end
-                    1000: begin key_pushed = 'h2; encoded_row_q = 'd4; end
+            4'b0010: case (row_d)
+                    4'b0001: begin key_pushed = 'h0; encoded_row_q = 2'b00; end
+                    4'b0010: begin key_pushed = 'h8; encoded_row_q = 2'b01; end
+                    4'b0100: begin key_pushed = 'h5; encoded_row_q = 2'b10; end
+                    4'b1000: begin key_pushed = 'h2; encoded_row_q = 2'b11; end
                      default: key_pushed = 'h0;
                   endcase
-            0100: case (row_d)
-                    0001: begin key_pushed = 'hb; encoded_row_q = 'd1; end
-                    0010: begin key_pushed = 'h9; encoded_row_q = 'd2; end
-                    0100: begin key_pushed = 'h6; encoded_row_q = 'd3; end
-                    1000: begin key_pushed = 'h3; encoded_row_q = 'd4; end
+            4'b0100: case (row_d)
+                    4'b0001: begin key_pushed = 'hb; encoded_row_q = 2'b00; end
+                    4'b0010: begin key_pushed = 'h9; encoded_row_q = 2'b01; end
+                    4'b0100: begin key_pushed = 'h6; encoded_row_q = 2'b10; end
+                    4'b1000: begin key_pushed = 'h3; encoded_row_q = 2'b11; end
                      default: key_pushed = 'h0;
                   endcase
-            1000: case (row_d)
-                    0001: begin key_pushed = 'hf; encoded_row_q = 'd1; end
-                    0010: begin key_pushed = 'he; encoded_row_q = 'd2; end
-                    0100: begin key_pushed = 'hd; encoded_row_q = 'd3; end
-                    1000: begin key_pushed = 'hc; encoded_row_q = 'd4; end
+            4'b1000: case (row_d)
+                    4'b0001: begin key_pushed = 'hf; encoded_row_q = 2'b00; end
+                    4'b0010: begin key_pushed = 'he; encoded_row_q = 2'b01; end
+                    4'b0100: begin key_pushed = 'hd; encoded_row_q = 2'b10; end
+                    4'b1000: begin key_pushed = 'hc; encoded_row_q = 2'b11; end
                      default: key_pushed = 'h0;
                   endcase
             default: key_pushed = 'h0;
         endcase
 
-        if (encoded_row_q == 'd1) row_q = row_d[0];
-        else if (encoded_row_q == 'd2) row_q = row_d[1];
-        else if (encoded_row_q == 'd3) row_q = row_d[2];
-        else if (encoded_row_q == 'd4) row_q = row_d[3];
+        if (encoded_row_q == 2'b00) row_q = row_d[0];
+        else if (encoded_row_q == 2'b01) row_q = row_d[1];
+        else if (encoded_row_q == 2'b10) row_q = row_d[2];
+        else if (encoded_row_q == 2'b11) row_q = row_d[3];
         else row_q = row_d[0];
     end
 
--- tb/scanner_fsm_tb.sv
+++ tb/scanner_fsm_tb.sv
@@ -4,7 +4,7 @@ module scanner_fsm_tb();
     logic clk, nrst;
     logic [3:0] row_d;
     logic [3:0] col_q_out, col_q, row_q, col_q_out_expected, col_q_expected, row_q_expected;
-    logic [31:0] vectornum, errors;
+    logic [65535:0] vectornum, errors;
     logic [15:0] testvectors [10000:0];
 
     scanner_fsm dut(clk, nrst, row_d, col_q_out, col_q, row_q);
@@ -18,9 +18,9 @@ module scanner_fsm_tb();
  //  - Pulse the reset line (if applicable)
  initial
    begin
-     $readmemb("scanner_fsm_tv.txt", testvectors, 0, `N_TV - 1);
+     $readmemb("scanner_fsm_tv.tv", testvectors, 0);
      vectornum = 0; errors = 0;
-     nrst = 0; #27; nst = 1;
+     nrst = 0; #27; nrst = 1;
    end
   // Apply test vector on the rising edge of clk
  always @(posedge clk)
@@ -36,11 +36,11 @@ module scanner_fsm_tb();
   // Check results on the falling edge of clk
  always @(negedge clk)
    begin
-     if (~reset) // skip during reset
+     if (~nrst) // skip during reset
        begin
          if (col_q_out != col_q_out_expected || col_q != col_q_expected || row_q != row_q_expected)
            begin
-             $display("Error: inputs: nrst=%b, row_d=%b", rst, row_d);
+             $display("Error: inputs: nrst=%b, row_d=%b", nrst, row_d);
              $display(" outputs: col_q_out=%b (%b expected), col_q=%b (%b expected), row_q=%b (%b expected)", col_q_out, col_q_out_expected, col_q, col_q_expected, row_q, row_q_expected);
              errors = errors + 1;
            end

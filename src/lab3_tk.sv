a096088f2de2eb907baaf445b4fb5e57d869c100
Troy Kaufman <tkaufman@hmc.edu>
2024-09-16 23:10:11 -0700

    No longer getting S0 state infinitely

--- src/lab3_tk.sv
+++ src/lab3_tk.sv
@@ -5,9 +5,9 @@
 module lab3_tk(input logic nrst,
                input logic [3:0] row_d,
                output logic [3:0] toggle_col,
-               output logic hex1_num, hex2_num,
-               output logic [6:0] seg,
-			   output logic debug_led);
+               output logic en_right, en_left,
+               output logic [6:0] seg);
+			  // output logic debug_led);
     
     logic int_osc;
     
@@ -19,7 +19,7 @@ module lab3_tk(input logic nrst,
 	logic pulse_en;
 	logic [3:0] current_num, prev_num;
 	logic current_hex;
-	logic en_right, en_left;
+	//logic en_right, en_left;
 	logic [3:0] sync_data;
 	
     // Bit swizzling
@@ -33,7 +33,7 @@ module lab3_tk(input logic nrst,
     synchronizer sync2(.clk(int_osc), .nrst(nrst), .data_d(row_d[2]), .data_q(sync_data2));
     synchronizer sync3(.clk(int_osc), .nrst(nrst), .data_d(row_d[3]), .data_q(sync_data3));
 
-    scanner_fsm scan(.clk(int_osc), .nrst(nrst), .row_d(sync_data), .col_q_out(toggle_col), .col_q(column_signals), .row_q(row_signals), .debug_led(debug_led));
+    scanner_fsm scan(.clk(int_osc), .nrst(nrst), .row_d(sync_data), .col_q_out(toggle_col), .col_q(column_signals), .row_q(row_signals)); //.debug_led(debug_led));
     keypad_decoder keydec(.col_d(column_signals), .row_d(row_signals), .row_q(row_pressed), .key_pushed(key_pushed));
     debouncer_fsm dbnc(.clk(int_osc), .nrst(nrst), .row_d(row_pressed), .pulse_en(pulse_en));
     slide_state slide(.clk(int_osc), .nrst(nrst), .pulse_en(pulse_en), .key_pushed(key_pushed), .current_num(current_num), .prev_num(prev_num));
--- src/scanner_fsm.sv
+++ src/scanner_fsm.sv
@@ -8,12 +8,12 @@
 */
 module scanner_fsm (input logic clk, nrst,
                     input logic [3:0] row_d,
-                    output logic [3:0] col_q_out, col_q, row_q,
-					output logic debug_led);
+                    output logic [3:0] col_q_out, col_q, row_q);
+					//output logic debug_led);
 					
-	assign debug_led = counter[20];
+	//assign debug_led = counter[20];
 
-    typedef enum logic [3:0] {row0_base = 0, row0_pressed = 1, row0_released = 2, row1_base = 3, row1_pressed = 4, row1_released = 5, row2_base = 6, row2_pressed = 7, row2_released =8, row3_base = 9, row3_pressed = 10, row3_released = 11} statetype;
+    typedef enum logic [3:0] {row0_base = 4'd0, row0_pressed = 4'd1, row0_released = 4'd2, row1_base = 4'd3, row1_pressed = 4'd4, row1_released = 4'd5, row2_base = 4'd6, row2_pressed = 4'd7, row2_released = 4'd8, row3_base = 4'd9, row3_pressed = 4'd10, row3_released = 4'd11} statetype;
     statetype current_state, next_state;
 
     //Counter to toggle the columns at 10 kHz
@@ -25,60 +25,62 @@ module scanner_fsm (input logic clk, nrst,
             current_state <= row0_base;
             counter <= 0;
         end
-        else if (counter == 20'b10010110000) begin
+        //else if (counter == 20'b10010110000) begin
+	/*else if (counter == 20'd20) begin
             current_state <= next_state;
             counter <= 0;
-        end
+        end */
         else begin
-            counter <= counter + 1;
+            //counter <= counter + 1;
+			current_state <= next_state;
         end
 
     //Next State Logic
     always_comb begin
         case(current_state)
             row0_base: //row0
-                if (row_d[0] == 1)
+                if (row_d[0] == 1'b1)
                     next_state = row0_pressed;
                 else 
                     next_state = row1_base;
             row0_pressed:
-                if (row_d[0] == 1)
+                if (row_d[0] == 1'b1)
                     next_state = current_state;
                 else
                     next_state = row0_released;
             row0_released:
                     next_state = row0_base;
             row1_base: //row1
-                if (row_d[1] == 1)
+                if (row_d[1] == 1'b1)
                     next_state = row1_pressed;
                 else 
                     next_state = row2_base;
             row1_pressed:
-                if (row_d[1] == 1)
+                if (row_d[1] == 1'b1)
                     next_state = current_state;
                 else
                     next_state = row1_released;
             row1_released:
                     next_state = row1_base;
             row2_base: //row2
-                if (row_d[2] == 1)
+                if (row_d[2] == 1'b1)
                     next_state = row2_pressed;
                 else 
                     next_state = row3_base;
             row2_pressed:
-                if (row_d[2] == 1)
+                if (row_d[2] == 1'b1)
                     next_state = current_state;
                 else
                     next_state = row2_released;
             row2_released:
                     next_state = row2_base;
             row3_base: //row3
-                if (row_d[3] == 1)
+                if (row_d[3] == 1'b1)
                     next_state = row3_pressed;
                 else 
                     next_state = row0_base;
             row3_pressed:
-                if (row_d[3] == 1)
+                if (row_d[3] == 1'b1)
                     next_state = current_state;
                 else
                     next_state = row3_released;
@@ -93,23 +95,23 @@ module scanner_fsm (input logic clk, nrst,
     //Output logic
     always_comb begin
         case(current_state)
-            row0_base: begin col_q_out = 4'b0001; col_q = 'b0000; end
-            row0_pressed: begin col_q_out = 4'b0000; col_q = 'b0001; end
-            row0_released: begin col_q_out = 4'b0000; col_q = 'b0000; end
+            row0_base: begin col_q_out = 4'b0001; col_q = 4'b0000; end
+            row0_pressed: begin col_q_out = 4'b0000; col_q = 4'b0001; end
+            row0_released: begin col_q_out = 4'b0000; col_q = 4'b0000; end
 
-            row1_base: begin col_q_out = 4'b0010; col_q = 'b0000; end
-            row1_pressed: begin col_q_out = 4'b0000; col_q = 'b0010; end
-            row1_released: begin col_q_out = 4'b0000; col_q = 'b0000; end
+            row1_base: begin col_q_out = 4'b0010; col_q = 4'b0000; end
+            row1_pressed: begin col_q_out = 4'b0000; col_q = 4'b0010; end
+            row1_released: begin col_q_out = 4'b0000; col_q = 4'b0000; end
 
-            row2_base: begin col_q_out = 4'b0100; col_q = 'b0000; end
-            row2_pressed: begin col_q_out = 4'b0000; col_q = 'b0100; end
-            row2_released: begin col_q_out = 4'b0000; col_q = 'b0000; end
+            row2_base: begin col_q_out = 4'b0100; col_q = 4'b0000; end
+            row2_pressed: begin col_q_out = 4'b0000; col_q = 4'b0100; end
+            row2_released: begin col_q_out = 4'b0000; col_q = 4'b0000; end
 
-            row3_base: begin col_q_out = 4'b1000; col_q = 'b0000; end
-            row3_pressed: begin col_q_out = 4'b0000; col_q = 'b1000; end
-            row3_released: begin col_q_out = 4'b0000; col_q = 'b0000; end
+            row3_base: begin col_q_out = 4'b1000; col_q = 4'b0000; end
+            row3_pressed: begin col_q_out = 4'b0000; col_q = 4'b1000; end
+            row3_released: begin col_q_out = 4'b0000; col_q = 4'b0000; end
 
-            default: begin col_q_out = 4'b0001; col_q = 4'b0000; end
+            default: begin col_q_out = 4'b0001; col_q = 4'b0001; end
         endcase
         row_q = row_d;
     end    

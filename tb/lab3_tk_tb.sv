9b7cc8ff2a3d85695b3d137b0438e6109aab9aef
Troy Kaufman <tkaufman@hmc.edu>
2024-09-22 15:28:14 -0700

    Flipped I/O in system to account for pull-up design. Design still doesn't work

--- tb/lab3_tk_tb.sv
+++ tb/lab3_tk_tb.sv
@@ -1,27 +1,27 @@
 `timescale 1ns/1ns
 
 module lab3_tk_tb();
-    logic int_osc, nrst;
+    logic new_clock, nrst;
     logic [3:0] row_d, column_signals;
     logic en_right, en_left;
     logic [6:0] seg;
 
-    lab3_tk dut(int_osc, nrst, row_d, column_signals ,en_right, en_left, seg);
+    lab3_tk dut(new_clock, nrst, row_d, column_signals ,en_right, en_left, seg);
 
 	always begin
-		int_osc = 1; #5; int_osc = 0; #5;
+		new_clock = 1; #5; new_clock = 0; #5;
 	end
 
     initial begin
         nrst = 0; #22; nrst = 1; #5;
 
-        row_d = 4'b0000; #120;
-	row_d = 4'b0001; #550;
-	row_d = 4'b0000; #30;
-	row_d = 4'b010; #613;
-	row_d = 4'b0000; #23;
-	row_d = 4'b0100; #650;
-	row_d = 4'b0000;
+        row_d = 4'b1111; #120;
+	row_d = 4'b1110; #550;
+	row_d = 4'b1111; #30;
+	row_d = 4'b1011; #613;
+	row_d = 4'b1111; #23;
+	row_d = 4'b1101; #650;
+	row_d = 4'b1111;
 
     end
 endmodule

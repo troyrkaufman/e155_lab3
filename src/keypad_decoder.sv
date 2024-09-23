3ddf57ff51d5878d71f4737ebe4711ba9b55f14d
Troy Kaufman <tkaufman@hmc.edu>
2024-09-22 23:41:43 -0700

    It works...IT ACTUALLY WORKSSSSSgit add .git add .git add . :)

--- src/keypad_decoder.sv
+++ src/keypad_decoder.sv
@@ -1,17 +1,27 @@
 
 
-module keypad_decoder(input logic [3:0] row_d,
-                      output logic row_bit);
+module keypad_decoder(input logic [3:0] row_d, col_q,
+		      output logic row_bit,
+		      output logic [3:0] key_pushed);
 
-  //  logic [1:0] encoded_row_q;
 	always_comb
-	if (row_d[0] == 1'b1)
-		row_bit = row_d[0];
-	else if (row_d[1] == 1'b1)
-		row_bit = row_d[1];
-	else if (row_d[2] == 1'b1)
-		row_bit = row_d[2];
-	else if (row_d[3] == 1'b1)
-		row_bit = row_d[3];
-	else row_bit = 1'b0;
+		case ({col_q, row_d})
+			8'b00010001: begin key_pushed = 'h1; row_bit = row_d[0]; end
+			8'b00010010: begin key_pushed = 'h4; row_bit = row_d[1]; end
+			8'b00010100: begin key_pushed = 'h7; row_bit = row_d[2]; end
+			8'b00011000: begin key_pushed = 'hE; row_bit = row_d[3]; end
+			8'b00100001: begin key_pushed = 'h2; row_bit = row_d[0]; end
+			8'b00100010: begin key_pushed = 'h5; row_bit = row_d[1]; end
+			8'b00100100: begin key_pushed = 'h8; row_bit = row_d[2]; end
+			8'b00101000: begin key_pushed = 'h0; row_bit = row_d[3]; end
+			8'b01000001: begin key_pushed = 'h3; row_bit = row_d[0]; end
+			8'b01000010: begin key_pushed = 'h6; row_bit = row_d[1]; end
+			8'b01000100: begin key_pushed = 'h9; row_bit = row_d[2]; end
+			8'b01001000: begin key_pushed = 'hF; row_bit = row_d[3]; end
+			8'b10000001: begin key_pushed = 'hA; row_bit = row_d[0]; end
+			8'b10000010: begin key_pushed = 'hB; row_bit = row_d[1]; end
+			8'b10000100: begin key_pushed = 'hC; row_bit = row_d[2]; end
+			8'b10001000: begin key_pushed = 'hD; row_bit = row_d[3]; end
+			default: begin key_pushed = 'hx; row_bit = 1'b0; end
+		endcase
 endmodule
